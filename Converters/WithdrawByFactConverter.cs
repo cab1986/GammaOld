5f0d068ac9a51c2fbe97312f7199727af362dc99
Alexandr Pavlov <pavlovav@rambler.ru>
2020-01-22 13:25:04 +0300

    В рапорте закрытия смены изменен вывод колонки Списание

---
+++ Converters/WithdrawByFactConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Gamma.Converters
+{
+    class WithdrawByFactConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return "По факту";
+            else
+                return "По нормативу";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "По факту")
+                return true;
+            else
+                return false;
+            //throw new NotImplementedException()
+        }
+    }
+}
