d6024bd47ad424d61dee6a4e3caab2d2279b537d
Alexandr Pavlov <pavlovav@rambler.ru>
2020-11-05 17:04:52 +0300

    Дополнение к предыдущему коммиту

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Windows;
 using Gamma.Common;
+using System;
 
 namespace Gamma.ViewModels
 {
@@ -89,6 +90,29 @@ namespace Gamma.ViewModels
                 GammaSettings.Get().UseScanner = false;
             }
             else GammaSettings.Get().UseScanner = UseScanner;
+
+            var checkResult = DB.CheckCurrentVersion();
+            var resultMessage = checkResult?.ResultMessage;
+            if (checkResult == null || !(string.IsNullOrWhiteSpace(resultMessage) && !checkResult.BlockCreation))
+            {
+                if (checkResult.BlockCreation)
+                {
+                    MessageBox.Show(resultMessage, "Проверка версии программы", MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    return;
+                }
+                else
+                {
+                    if (checkResult == null)
+                        resultMessage = "Ошибка при проверке версии программы. (" + GammaSettings.Version + ")";
+                    var dialogResult = MessageBox.Show(resultMessage + Environment.NewLine + " Продолжить работу?", "Проверка версии программы", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    if (dialogResult == MessageBoxResult.No)
+                    {
+                        return;
+                    }
+
+                }
+            }
             MessageManager.OpenMain();
             CloseWindow();
         }
