243a3d548d8d4bc8c0ff6d0671a24d4c63a7b8d2
user <user@DESKTOP-6VTQH0F>
2022-09-13 09:02:51 +0300

    1. (28641) Добавлена возмжность массового изменения виновника в актах о браке. 2. В актах о браке добавлен столбец Качество.

---
+++ Models/BrokePlace.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gamma.Common;
+using Gamma.ViewModels;
+using DevExpress.Mvvm;
+
+
+namespace Gamma.Models
+{
+    public class BrokePlace : DbEditItemWithNomenclatureViewModel
+    {
+        public BrokePlace()
+        {
+            BrokePlaceList = WorkSession.Places.Select(p => new Place
+               {
+                   PlaceID = p.PlaceID,
+                   PlaceName = p.Name
+               })
+               .ToList();
+
+            BrokeShiftsList = new List<KeyValuePair<byte, string>>();
+            BrokeShiftsList.Add(new KeyValuePair<byte, string>(0, "Не сменный"));
+            foreach (var shiftItem in WorkSession.Shifts)
+            {
+                BrokeShiftsList.Add(new KeyValuePair<byte, string>(shiftItem.ShiftID, shiftItem.Name));
+            }
+            
+        }
+
+
+
+
+        public string ForAllProductBrokeFIO { get; set; }
+
+        public List<Place> BrokePlaceList { get; set; }
+        private int? _placeID;
+
+        public int? PlaceID
+        {
+            get { return _placeID; }
+            set
+            {
+                _placeID = value;
+                RaisePropertyChanged("PlaceID");
+            }
+        }
+        private byte? _shiftID { get; set; }
+        public byte? ForAllProductBrokeShiftID
+        {
+            get { return _shiftID; }
+            set
+            {
+                _shiftID = value;
+                RaisePropertyChanged("ForAllProductBrokeShiftID");
+            }
+        }
+        private KeyValuePair<int, string> _shiftStateID { get; set; }
+        public KeyValuePair<int, string> ShiftStateID
+        {
+            get { return _shiftStateID; }
+            set
+            {
+                _shiftStateID = value;
+                RaisePropertyChanged("ShiftStateID");
+            }
+        }
+        public List<Place> ProductPlaceList { get; private set; }
+        private List<KeyValuePair<byte, string>> _brokeShiftsList { get; set; }
+        public List<KeyValuePair<byte, string>> BrokeShiftsList
+        {
+            get { return _brokeShiftsList; }
+            set
+            {
+                _brokeShiftsList = value;
+                RaisePropertyChanged("BrokeShiftsList");
+            }
+        }
+
+
+
+
+        
+    }
+}
